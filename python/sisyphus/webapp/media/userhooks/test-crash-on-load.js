1a3e9e7c4a13e99ed6a4fa1bd5d1cbb337f8cb0a
Bob Clary <bclary@bclary.com>
2012-02-01 11:45:30 -0800

    Sisyphus - Crash Automation - trivial reorg of userhook test-crash-on-load.js, no bug, r=self.

--- python/sisyphus/webapp/media/userhooks/test-crash-on-load.js
+++ python/sisyphus/webapp/media/userhooks/test-crash-on-load.js
@@ -1,20 +1,35 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
 /*
- * top-sites user hook function: disable dialogs and complete on page load.
+ * top-sites user hook function:
+ *
+ * - disable dialogs on start,
+ * - wait for the specified period after each page loads,
+ * - load the specified number of linked pages before stopping,
+ * - enable dialogs on stop.
  */
 
 var gPageCount = 0;
 var gPageLimit = 1;
+var gPageWait  = 1 * 1000;
 
-function userOnBeforePage()
+function userOnStart()
 {
   registerDialogCloser();
 }
 
+function userOnStop()
+{
+  unregisterDialogCloser();
+}
+
+function userOnBeforePage()
+{
+}
+
 function userOnAfterPage()
 {
-  completePage();
+  setTimeout(completePage, gPageWait);
 }
 
 function completePage()
@@ -29,6 +44,5 @@ function completePage()
   }
 
   gPageCompleted = true;
-  unregisterDialogCloser();
 }
 
